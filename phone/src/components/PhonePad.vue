52c9a5c1dc88b7e7ffbded32e831b91ee23dd2cc
ars <ars.anosov@gmail.com>
2025-11-12 14:27:06 +0300

    v-row fix.

--- phone/src/components/PhonePad.vue
+++ phone/src/components/PhonePad.vue
@@ -8,44 +8,20 @@
     <!-- <v-divider></v-divider> -->
     <v-form @submit.prevent="handleSubmit" @reset.prevent="handleReset">
     <v-row v-if="props.showInput">
-      <v-col cols="12" sm="9">
+      <v-col cols="7" sm="9">
         <v-text-field
           v-model="sip.calleePhoneNum"
           label="98..."
         ></v-text-field>
       </v-col>
-      <v-col cols="12" sm="3" class="text-right">
+      <v-col cols="5" sm="3" class="text-left">
         <v-btn icon="mdi-arrow-left-bold-outline" @click="sip.deleteLast"></v-btn>
       </v-col>
     </v-row>
 
-    <v-card v-if="props.showInput"
-      elevation="0"
-      class="mx-auto pa-4"
-      max-width="270"
-    >
-      <v-row>
-        <v-col
-          v-for="key in keys"
-          :key="key[0]"
-          cols="12" sm="4" class="text-center"
-        >
-          <v-btn
-            height="40"
-            @click="sip.appendKey(key[0])"
-          >
-            <span>
-              <div class="mb-1">{{ key[0] }}</div>
-              <small class="text-medium-emphasis">{{ key[1] }}</small>
-            </span>
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-card>
-
     <v-row>
-      <v-col cols="12" sm="12" class="text-right">
-        <v-btn class="ma-2"
+      <v-col cols="6" sm="6" class="text-center">
+        <v-btn
           color="success"
           type="submit"
           icon
@@ -54,7 +30,9 @@
           <v-icon>mdi-phone</v-icon>
           <v-tooltip activator="parent" location="top">{{ sip.incomeDisplay ? 'Answer' : 'Call' }}</v-tooltip>
         </v-btn>
-        <v-btn class="ml-2"
+      </v-col>
+      <v-col cols="6" sm="6" class="text-center">
+        <v-btn
           color="error"
           type="reset"
           icon
@@ -62,6 +40,34 @@
           <v-icon>mdi-phone-hangup</v-icon>
           <v-tooltip activator="parent" location="top">End</v-tooltip>
         </v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row v-if="props.showInput">
+      <v-col cols="12" sm="12">
+        <v-card
+          elevation="0"
+          class="mx-auto pa-4"
+          max-width="270"
+        >
+          <v-row>
+            <v-col
+              v-for="key in keys"
+              :key="key[0]"
+              cols="4" sm="4" class="text-center"
+            >
+              <v-btn
+                height="40"
+                @click="sip.appendKey(key[0])"
+              >
+                <span>
+                  <div class="mb-1">{{ key[0] }}</div>
+                  <small class="text-medium-emphasis">{{ key[1] }}</small>
+                </span>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
     </v-row>
     
